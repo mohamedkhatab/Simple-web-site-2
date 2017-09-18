4af92b4c6af4913c6f881061640b8db4fde7cb9b
mohamed khatab <hamosuper8@gmail.com>
2017-09-18 14:19:43 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+   // $(".about").click(function(){
+        
+//$(".home").fadeOut();
+
+   // });
+    
+    $(".about").click(function(){
+
+$("body,html").animate({
+screenTop: $(".csst").offset().top
+},500);
+
+});
+    
+
+
+});
+
